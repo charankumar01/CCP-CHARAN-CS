0d0afe2793290a8e53e186555ecd5a503285b673
Charan Kumar <khatricharan4@gmail.com>
2026-01-10 10:24:42 +0500

    feature/Name

---
+++ Name.java
@@ -0,0 +1,14 @@
+public class Name {
+    private String value;
+
+    public Name(String value) {
+        if (value == null || value.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
